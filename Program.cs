83e57c25dafa4e1c0ce699d0e46a56eac68a11c0
Pavan <vpavankumar1999@gmail.com>
2024-02-16 10:12:02 +0530

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalcExample;
+
+Console.WriteLine("Hello, World!");
+
+Calc calc = new Calc();
+Console.WriteLine("Add Call");
+calc.Add(10, 5);
+
+Console.WriteLine("Sub Call");
+calc.Sub(10, 5);
+
+Console.WriteLine("Div Call");
+calc.Div(10, 5);
+
+Console.WriteLine("Multi Call");
+calc.Multi(10, 5);
